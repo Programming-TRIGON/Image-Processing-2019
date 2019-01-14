32c3b73d782d1e4c11ed1ac7c7936b07490c48c5
esh4 <eshel.sinclair@gmail.com>
2019-01-14 18:03:37 +0200

    added git ignore

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from ovl import Color
 from ovl import Filters
 import cv2
 
+CAMERA_PORT = 1
 
 color_cargo_hsv = Color.Color(low=[0, 122, 50], high=[34, 255, 255])
-v = Vision.Vision(camera_port=1, color=color_cargo_hsv)
+v = Vision.Vision(camera_port=CAMERA_PORT, color=color_cargo_hsv)
 
+cam = cv2.VideoCapture(CAMERA_PORT)
+_, img = cam.read()
+cam.release()
 
+v.get_contours(img=img)
+v.apply_filter(filter_function=Filters.circle_filter)
+v.display_contours(img=img)
