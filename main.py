94c8de1bb5a5f57cdb9af3f0cfa1a606dbb8fe84
esh4 <eshel.sinclair@gmail.com>
2019-01-14 18:33:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from ovl import Color
 from ovl import Filters
 import cv2
 
-CAMERA_PORT = 1
+CAMERA_PORT = 0
 
-color_cargo_hsv = Color.Color(low=[0, 122, 50], high=[34, 255, 255])
+color_cargo_hsv = Color.Color(low=[0, 103, 83], high=[56, 255, 255])
 v = Vision.Vision(camera_port=CAMERA_PORT, color=color_cargo_hsv)
 
-cam = cv2.VideoCapture(CAMERA_PORT)
-_, img = cam.read()
-cam.release()
+conts, img = v.apply_sample(camera_port=CAMERA_PORT)
+print 'found {} contours'.format(len(conts))
+cv2.imshow('', img)
+cv2.waitKey()
+v.display_contours(img)
 
-v.get_contours(img=img)
-v.apply_filter(filter_function=Filters.circle_filter)
-v.display_contours(img=img)
+
+
