de39b166e61930a63c3ee3734f1d6b2abf4591d8
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-15 01:11:45 +0200

    last comit 14.1

--- HatchVisionProcessing.py
+++ HatchVisionProcessing.py
@@ -4,17 +4,16 @@ from ovl import Filters
 from TargetFinder import TargetFinder
 
 
-#class HatchVisionProcessing(TargetFinder):
+class HatchVisionProcessing(TargetFinder):
 
- #   def __init__(self):
-  #      HatchColor = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
-   #     self.v = Vision.Vision(camera_port=0, color=HatchColor, filters=[Filters.circle_filter, self.examplefileter], parameters=[[], [1,2]], )
+    def __init__(self):
+        HatchColor = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
+        self.v = Vision.Vision(camera_port=0, color=HatchColor, filters=[Filters.circle_filter, self.examplefileter], parameters=[[], [1,2]], )
 
 
-    #def enable(self):
-     #   pass
-    #def examplefileter(self, contores, a,b):
-
-     #   pass
+    def enable(self):
+        pass
+    def examplefileter(self, contores, a,b):
+        pass
 
 
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ CAMERA_PORT = 0
 
 some_color = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
 
-v = Vision.Vision(camera_port=r'D:\Downloads\img.jpg', color=some_color, filters=[Filters.area_filter()])
+v = Vision.Vision(camera_port=r'D:\Downloads\img.jpg', color=some_color, filters=[Filters.area_filter(), ])
 
 conts, img = v.apply_sample(camera_port='D:\Downloads\img.jpg')
 print('found {} contours'.format(len(conts)))
