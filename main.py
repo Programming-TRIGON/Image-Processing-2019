8b401ae5e5c6dd1ab99eb748e168668b034e9e6e
esh4 <eshel.sinclair@gmail.com>
2019-01-14 18:36:52 +0200

    with circle filter

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import cv2
 CAMERA_PORT = 0
 
 color_cargo_hsv = Color.Color(low=[0, 103, 83], high=[56, 255, 255])
-v = Vision.Vision(camera_port=CAMERA_PORT, color=color_cargo_hsv)
+
+v = Vision.Vision(camera_port=CAMERA_PORT, color=color_cargo_hsv, filters=[Filters.circle_filter])
 
 conts, img = v.apply_sample(camera_port=CAMERA_PORT)
 print 'found {} contours'.format(len(conts))
