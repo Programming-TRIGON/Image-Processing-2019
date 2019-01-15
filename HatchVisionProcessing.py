91848e07165d96cf119b4ea287b5b76081aef1b3
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-15 19:55:16 +0200

    updated hatch filter

--- HatchVisionProcessing.py
+++ HatchVisionProcessing.py
@@ -1,3 +1,5 @@
+from dis import code_info
+
 from ovl import Color
 from ovl import Vision
 from ovl import Filters
@@ -8,12 +10,15 @@ class HatchVisionProcessing(TargetFinder):
 
     def __init__(self):
         HatchColor = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
-        self.v = Vision.Vision(camera_port=0, color=HatchColor, filters=[Filters.circle_filter, self.examplefileter], parameters=[[], [1,2]], )
+        self.v = Vision.Vision(camera_port=0, color=HatchColor, filters=[Filters.area_filter(min_area=200, max_area=3000), self.SolidityFileter], parameters=[[], [1,2]], )
 
+    def solidityFileter(self, contour_list, solidity):
+
+
+        return contour_list
 
     def enable(self):
         pass
-    def examplefileter(self, contores, a,b):
+
+    def  disable(self):
         pass
-
-
