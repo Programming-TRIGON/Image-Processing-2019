a8bc3bb6fec801a020b758fd2783c4103b20bf4e
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-16 22:36:38 +0200

    final filter

--- test.py
+++ test.py
@@ -2,12 +2,17 @@ from ovl_eshel.Code import Vision
 from ovl_eshel.Code import Color
 from ovl_eshel.Code import Filters
 import cv2
-import HatchFinder
 
+
+def size_filter(contour_list):
+    output = contour_list
+    output.sort(key=lambda contour: cv2.contourArea(contour))
+    return output[len(output)-1::]
 
 def solidity_filter(contour_list, solidity):
     output = []
     for contour in contour_list:
+
         area = cv2.contourArea(contour)
         hull = cv2.convexHull(contour)
         solid = 100 * area / cv2.contourArea(hull)
@@ -22,7 +27,7 @@ CAMERA_PORT = 0
 some_color = Color.Color(low=[21, 131, 124], high=[27, 255, 255])
 
 v = Vision.Vision(camera_port=1, color=some_color,
-                  filters=[Filters.area_filter], parameters=[[200]])
+                  filters=[Filters.area_filter, size_filter], parameters=[[200], []])
 
 conts, img = v.apply_sample(camera_port=1)
 print('found {} contours'.format(len(conts)))
