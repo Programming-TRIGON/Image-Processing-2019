7d7cfc49eed1d77cc738bf3426495b8a8e5a2e09
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-16 18:35:02 +0200

    tabed ehsel.vision properly

--- VisionTest.py
+++ VisionTest.py
@@ -1,8 +1,13 @@
-from ovl import Color
-from ovl import Vision
-from ovl import Filters
+from ovl_eshel.Code import Vision
+from ovl_eshel.Code import Color
+from ovl_eshel.Code import Directions
+from ovl_eshel.Code import Filters
+import cv2
 
-ColorToTest= Color.Color(low=[0, 28, 138], high=[40, 255, 255])
-v = Vision.Vision(camera_port='D:\Downloads\img.jpg', color=ColorToTest, filters=[Filters.circle_filter])
-v.display_contours()
-
+ColorToTest = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
+v = Vision.Vision(camera_port='D:\Downloads\img.jpg', color=ColorToTest, filters=[Filters.area_filter], parameters=[[200]], directions_function=Directions.x_center_directions, target_amount=4,)
+conts, img = v.apply_sample(camera_port='D:\Downloads\img.jpg')
+print('found {} contours'.format(len(conts)))
+cv2.imshow('', img)
+cv2.waitKey()
+v.display_contours(img)
