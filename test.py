4a5e1beffece5b836e280c041fb0234bbda21559
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-01-16 20:14:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ CAMERA_PORT = 0
 
 some_color = Color.Color(low=[0, 28, 138], high=[40, 255, 255])
 
-v = Vision.Vision(camera_port=0, color=some_color)
+v = Vision.Vision(camera_port=1, color=some_color)
 
-conts, img = v.apply_sample(camera_port=0)
+conts, img = v.apply_sample(camera_port=1)
 print('found {} contours'.format(len(conts)))
 cv2.imshow('',img)
 v.display_contours(img)
